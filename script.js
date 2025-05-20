cd130fbea4c326c1ad4c254d2a359cd4316d56a4
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-20 17:05:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+//your JS code here. If required.{
+.reset("")	
+alert("Please enter valid details.");
+
+	let myPromise=new Promise((resolve,reject)=>{
+		setTimeOut(function(age) {
+			if(age>18){
+	  alert("Welcome,You can vote");		
+			})
+		resolve()
+	},4000)
+	}).then function
+}
+	
