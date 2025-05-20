ef251a6546d437d9eaa37f928d88be2a38ed6052
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-20 22:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", function(e) {
         let myPromise = new Promise(function(resolve, reject) {
             setTimeout(function() {
                 if (parseInt(user1.value) >= 18) {
-                    resolve(`Welcome , ${user2.value} You can vote`);
+                    resolve(`Welcome , ${user2.value}You can vote`);
                 } else {
                     reject(`Oh sorry, ${user2.value}. You aren't old enough.`);
                 }
